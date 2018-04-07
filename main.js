df31f617b0f7c52a1c15b95dba7ec272e53d2c66
Mihir Pathak <mihirpathak97@gmail.com>
2018-04-07 19:53:34 +0530

    main.js: maximize and disbale resizability

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const url = require('url')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 1920, height: 1080, icon: path.join(__dirname, 'assets/icons/mac.icns')})
+    win = new BrowserWindow({icon: path.join(__dirname, 'assets/icons/mac.icns')});
+    win.maximize();
+    win.setResizable(false);
     // and load the index.html of the app.
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'src/index.html'),
