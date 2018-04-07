50e16b5d45f3190c3b76810ba928755c5fc86206
Mihir Pathak <mihirpathak97@gmail.com>
2018-04-07 19:57:29 +0530

    Change set #3
    * Add Sign Up Logic
    * Layout cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -24,6 +24,39 @@ function authCheck() {
     console.log(error);
     success = false;
   });
-
-
 }
+
+$('#signup').click(function () {
+  $('.w3-modal').show();
+});
+
+$('#signupSubmit').click(function () {
+  name = $('#signupName').val();
+  email = $('#signupEmail').val();
+  password = $('#signupPassword').val();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
+    firebase.auth().onAuthStateChanged(function(user) {
+      // If user is logged in
+      if (user) {
+        user.updateProfile({
+          displayName: name
+        }).then(function() {
+          remote.getCurrentWindow().loadURL(
+            url.format({
+              pathname: path.join(__dirname, 'home.html'),
+              protocol: 'file:',
+              slashes: true
+            }))
+        }).catch(function(error) {
+          // An error happened.
+        });
+      }
+      else {
+
+      } })
+    }).catch(function(error) {
+    console.log(error);
+  });
+
+});
